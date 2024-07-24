e3b559d13f5e348328019ee9f62beb526577d0a1
tomer <tomerkl22@gmail.com>
2024-07-24 22:56:27 +0300

    fixing Cors

--- src/main/java/com/example/codingwebapp/config/WebSocketConfig.java
+++ src/main/java/com/example/codingwebapp/config/WebSocketConfig.java
@@ -19,7 +19,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("https://onlinecodingwebapp-client.onrender.com")
+                .withSockJS();
     }
 
 }
